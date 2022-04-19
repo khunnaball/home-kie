ee6416eb93e580f5fd34bb4c2902a640a64ec1a2
khunnaball <kieranhunnaball@gmail.com>
2022-04-19 23:27:33 +0100

    Refactored bookmarks to allow config file usage

---
+++ src/components/Bookmark.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <button class="button" :id="name"><a :target="newTab ? '_blank' : ''" :key="id" :href="link">{{ name }}</a></button>
+        <p> | </p>
+    </div>
+</template>
+
+<script>
+import { config } from "../../config";
+
+export default {
+    props: {
+        name: String,
+        link: String,
+        id: String,
+    },
+    data () {
+        return {
+            newTab: config.openInNewTab
+        };
+    }
+}
+</script>
+
+<style scoped >
+div {
+    display: flex;
+}
+
+div:last-child > p:last-child {
+    display: none;
+}
+
+p {
+    font-family: 'SpaceMono-Regular';
+    -webkit-text-fill-color: #757575;
+    font-size: 12px;
+	z-index:1;
+}
+
+button {
+    all: unset;
+    cursor: pointer;
+    width: auto;
+    height: 10px;
+}
+
+.button {
+	border: none;
+    background: none;
+    color: #757575;
+    padding: 10px;
+    font-size: 16px;
+    align-content: center;
+    text-align: center;
+    font-family: 'SpaceMono-regular';
+}
+
+.button:hover {
+	border: none;
+    background: none;
+    color: #C5B3FD;
+    padding: 10px;
+    font-size: 16px;
+    align-content: center;
+    text-align: center;
+    font-family: 'SpaceMono-regular';
+}
+
+a {
+    color: inherit; 
+    text-decoration: none;
+}
+</style>
--- src/components/Bookmarks.vue
+++ src/components/Bookmarks.vue
@@ -1,30 +1,32 @@
 <template>
     <div class="button-container">
         <div class="button-row">
-            <button class="button" id="youtube"><a target="_self" href="https://youtube.com">youtube</a></button>
-            <p> | </p>
-            <button class="button" id="twitter"><a target="_self" href="https://twitter.com">twitter</a></button>
-            <p> | </p>
-            <button class="button" id="twitch"><a target="_self" href="https://www.twitch.tv/">twitch</a></button>
-            <p> | </p>
-            <button class="button" id="gmail"><a target="_self" href="https://mail.google.com/">gmail</a></button>
+            <Bookmark v-for="bookmark in bookmarksOne" :key="bookmark.id" :id="bookmark.id" :link="bookmark.link" :name="bookmark.name"/>
         </div>
         <div class="button-row">
-            <button class="button" id="github"><a target="_self" href="https://github.com">github</a></button>
-            <p> | </p>
-            <button class="button" id="reddit"><a target="_self" href="https://www.reddit.com/">reddit</a></button>
-            <p> | </p>
-            <button class="button" id="twitter"><a target="_self" href="https://dev.to">dev.to</a></button>
-            <p> | </p>
-            <button class="button" id="spotify"><a target="_self" href="https://spotify.com/">spotify</a></button>
-            <p> | </p>
-            <button class="button" id="discord"><a target="_self" href="https://discord.com/">discord</a></button>
+            <Bookmark v-for="bookmark in bookmarksTwo" :key="bookmark.id" :id="bookmark.id" :link="bookmark.link" :name="bookmark.name"/>
         </div>
     </div>
 </template>
 
-<style scoped>
+<script>
+import { config } from "../../config";
+import Bookmark from "./Bookmark.vue";
 
+export default {
+    components: {
+        Bookmark
+    },
+    data() {
+        return {
+            bookmarksOne: config.firstBookmarksList,
+            bookmarksTwo: config.secondBookmarksList
+        };
+    }
+}
+</script>
+
+<style scoped>
 .button-container {
     display: flex;
     flex-direction: column;
@@ -37,46 +39,5 @@
     display: flex;
     flex-direction: row;
     height: 50%;
-}
-
-p {
-    font-family: 'SpaceMono-Regular';
-    -webkit-text-fill-color: #757575;
-    font-size: 12px;
-	z-index:1;
-}
-
-button {
-    all: unset;
-    cursor: pointer;
-    width: auto;
-    height: 10px;
-}
-
-.button {
-	border: none;
-    background: none;
-    color: #757575;
-    padding: 10px;
-    font-size: 16px;
-    align-content: center;
-    text-align: center;
-    font-family: 'SpaceMono-regular';
-}
-
-.button:hover {
-	border: none;
-    background: none;
-    color: #C5B3FD;
-    padding: 10px;
-    font-size: 16px;
-    align-content: center;
-    text-align: center;
-    font-family: 'SpaceMono-regular';
-}
-
-a {
-    color: inherit; 
-    text-decoration: none;
 }
 </style>
